dd57240ae3f9cb170a20c0997a2b79038802e44a
Amit Carpenter <114389560+amitcarpenter@users.noreply.github.com>
2023-10-09 13:00:59 +0530

    Update isBlog.js

--- backend/middlewares/isBlog.js
+++ backend/middlewares/isBlog.js
@@ -3,6 +3,10 @@ const BlogSetting = require("../models/blogSettingModel");
 const isBlog = async (req, res, next) => {
   try {
     const blogSetting = await BlogSetting.find();
+      if(req.originalUrl == "/"){
+      res.redirect("/login")
+    }
+
     if (blogSetting.length == 0 && req.originalUrl != "/blog-setup") {
       res.redirect("/blog-setup");
     } else {
